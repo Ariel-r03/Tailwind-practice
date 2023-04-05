7d44bea31d759e20083c58e3611a157722cd4b95
Ariel-r03 <arieladolfo.silva@gmail.com>
2023-04-05 10:27:32 -0600

    Navbar design

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styles from "./style";
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer,
+} from "./components";
+
+function App() {
+  return (
+    <div className="bg-black w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`text-white ${styles.boxWidth}`}><Navbar/></div>
+      </div>
+      <div className={`bg-black ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}><Hero/></div>
+      </div>
+      <div className={`bg-black ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats/> <Business/> <Billing/> <CardDeal/> <Testimonials/> <Clients/> <CTA/> <Footer/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
